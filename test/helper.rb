cd63730a183e960146e8a7cc8372818ddf9ecffd
Dru Ibarra <Druwerd@gmail.com>
2012-03-19 17:53:35 -0700

    updated gem name

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'caplock'
+require 'gnm-caplock'
 
 class Test::Unit::TestCase
 end
